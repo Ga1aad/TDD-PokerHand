8a1406ffe81e8891fb10d1864ff67c8358b482a1
Ga1aad <136579419+Ga1aad@users.noreply.github.com>
2026-03-05 14:41:53 +0100

    feat: introduce core types for `Suit`, `Rank`, and `Card` to represent playing cards.

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,11 @@
-// P = Pique, C = Coeur, K = Carreaux, T = Trefle
-export type CardType = ["P", "C", "K", "T"];
-export type CardTypeColor = ["Noir", "Rouge", "Rouge", "Noir"];
+// P = Pique, C = Coeur, K = Carreau, T = Trefle
+export type Suit = "P" | "C" | "K" | "T";
 
-// 1 = 2, 2 = 3, ..., 11 = Valet, 12 = Dame, 13 = Roi, 14 = As
-export type Rank = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-export type RankType = [
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-  "A",
-];
+// Les valeurs numériques directes pour faciliter les tris (sort) et les comparaisons
+export type Rank = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
+
+// On assemble le tout pour créer la carte
+export interface Card {
+  rank: Rank;
+  suit: Suit;
+}
